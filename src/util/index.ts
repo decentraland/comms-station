3d81b02bfffcd577f602749756b50d056acff800
Santiago Lezica <salezica@gmail.com>
2023-05-10 18:19:04 -0300

    Remove unused stuff

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,21 +1,11 @@
 export * from './emitter'
 export * from './identity'
+export * from '../transports/base'
+export * from './mpromise'
 
 // lastOf returns the last element of an array, or null if empty.
 export function lastOf<T>(array: T[]): T | null {
   return array.length > 0 ? array[array.length - 1] : null
-}
-
-
-// setOnNextListener consumes a generator while invoking a callback for each item:
-export async function setOnNextListener<T>(gen: AsyncGenerator<T>, onNext: (item: T) => void) {
-  try {
-    for await (const item of gen) {
-      onNext(item)
-    }
-  } catch (error) {
-    throw error // TODO
-  }
 }
 
 // setAsyncInterval is like setInterval, but it awaits a Promise returned by the handler before
@@ -31,4 +21,3 @@ export function setAsyncInterval(f: () => Promise<void>, intervalMs: number) {
 
   onTimeout()
 }
-
