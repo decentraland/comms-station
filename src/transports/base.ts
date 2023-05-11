18c6a79fdd2b0b345c660ac94bc50607955fb043
Santiago Lezica <salezica@gmail.com>
2023-05-11 10:56:42 -0300

    Add some comments

--- src/transports/base.ts
+++ src/transports/base.ts
@@ -2,6 +2,12 @@ import { Emitter, Events } from "../util"
 
 export type TransportMessage = { $case: string }
 
+// TODO:
+// I don't like using the same $case for our connection/disconnection events and those of our peers.
+// This was one simplification too many. Ideally, the Event type should be extensible for subclasses
+// of Transport, but I couldn't get the type system to do that (I ended up with a disjointed union
+// where `T['$case']` is `never`).
+
 export type TransportEvent<Incoming extends TransportMessage> =
   | { $case: 'connected', peer?: string }                     // we or a peer just connected
   | { $case: 'message', peer?: string, message: Incoming }    // the server or a peer sent a message
@@ -21,16 +27,22 @@ export abstract class Transport<
   Outgoing extends TransportMessage,
 > extends Emitter<TransportEvent<Incoming>> {
 
+  // connect establishes a connection and performs any initial handshake.
   abstract connect(): Promise<void>
 
+  // send writes a message through the transport.
   abstract send(message: Outgoing): void
 
+  // disconnect breaks the connection (not immediately).
   abstract disconnect(): void
 
+  // receive returns a Promise for the next message of a given $case.
   async receive<C extends Incoming['$case']>($case: C): Promise<MessageEvent<Incoming, C>> {
     return (await this.receiveMany($case).next()).value!
   }
 
+   // TODO: rename this
+   // receiveMany returns an AsyncGenerator for future messages of a given $case.
   async* receiveMany<C extends Incoming['$case']>($case: C) {
     for await (let ev of Events.stream(this, 'message')) {
       if (ev.message.$case == $case) yield ev as MessageEvent<Incoming, C>
