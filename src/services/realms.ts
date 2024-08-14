7fa0d07a2ff644417cee197f0e03d7bbd84d4bc3
Matias Pentreath <pentreathm@decentraland.org>
2024-08-14 15:10:45 -0300

    fix: update url to retreive the list of realms

--- src/services/realms.ts
+++ src/services/realms.ts
@@ -9,7 +9,7 @@ export interface Realm {
 
 // findRealms uses the lambdas API to fetch a list of public realms:
 export async function findRealms() {
-  const res = await fetch('https://peer.decentraland.org/lambdas/explore/realms')
+  const res = await fetch('https://realm-provider.decentraland.org/realms')
   const obj = await res.json()
 
   return obj as Realm[]
